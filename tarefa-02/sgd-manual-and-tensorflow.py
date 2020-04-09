0270680973519ba4a6942e6a0919ea5397bd5c1a
Patrick de Carvalho Tavares Rezende Ferreira <ra175480@students.ic.unicamp.br>
2020-04-09 05:08:57 -0300

    Funcionalidades totais terminadas. Falta comentar, gerar relatório e talvez mais alguns gráficos.

--- tarefa-02/sgd-manual-and-tensorflow.py
+++ tarefa-02/sgd-manual-and-tensorflow.py
@@ -1,6 +1,7 @@
 from numpy.linalg import norm
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 
 def rosenbrock_3d(x):
@@ -61,26 +62,68 @@ def sgd_manual(lr=10 ** -3, max_passos=20000):
         if tolerancia(x, x_old) < 10 ** -4:
             plot_rosenbrock.append(rosenbrock_3d(x))
             break
-
+    plt.title("Gradiente manual com LR de " + str(lr))
     plt.xlabel("Número de atualizações de x")
     plt.ylabel("f(x) - Rosenbrock 3d")
     plt.plot(plot_rosenbrock)
     plt.show()
 
     print("\nNúmero de passos do gradiente: ", len(plot_rosenbrock))
+    print("\nValor da função no mínimo local: ", rosenbrock_3d(x))
+
+
+def sgd_tensorflow(lr=10 ** -3, max_passos=20000):
+    x1_old, x2_old, x3_old = tf.Variable(0), tf.Variable(0), tf.Variable(0)
+    x1, x2, x3 = tf.Variable(10 ** -10), tf.Variable(10 ** -10), tf.Variable(10 ** -10)
+
+    opt = tf.keras.optimizers.SGD(lr=lr)
+
+    lista_plot = list()
+
+    for i in range(max_passos):
+        with tf.GradientTape() as tape:
+            y = 100 * (x2 - x1 ** 2) ** 2 + (1 - x1) ** 2 + 100 * (x3 - x2 ** 2) ** 2 + (1 - x2) ** 2
+        grads = tape.gradient(y, [x1, x2, x3])
+        processed_grads = [g for g in grads]
+        grads_and_vars = zip(processed_grads, [x1, x2, x3])
+
+        lista_plot.append([y.numpy(), x1.numpy(), x2.numpy(), x3.numpy(), grads[0].numpy(), grads[1].numpy(), grads[2].numpy()])
+
+        x1_old, x2_old, x3_old = x1.numpy(), x2.numpy(), x3.numpy()
+        opt.apply_gradients(grads_and_vars)
+
+        if tolerancia([x1, x2, x3], [x1_old, x2_old, x3_old]) < 10 ** -4:
+            break
+
+    lista_plot = np.array(lista_plot)
+
+    plt.title("Gradiente tensorflow com LR de " + str(lr))
+    plt.xlabel("Número de atualizações de x")
+    plt.ylabel("f(x) - Rosenbrock 3d")
+    plt.plot(lista_plot[:, 0].reshape(lista_plot[:, 0].shape[0]))
+    plt.show()
+
+    print("\nNúmero de passos do gradiente: ", lista_plot.shape[0])
+    print("\nValor da função no mínimo local: ", rosenbrock_3d([x1, x2, x3]).numpy())
+
+    return lista_plot
 
 
 def main():
-    # Learning rate pequeno aumenta a chance de convergir, ao passo que diminui
-    # a velocidade em que se alcanca o minimo local.
-    sgd_manual(lr=1.00 * 10 ** -4)
+    # # Learning rate pequeno aumenta a chance de convergir, ao passo que diminui
+    # # a velocidade em que se alcanca o minimo local.
+    # sgd_manual(lr=1.00 * 10 ** -4)
+    #
+    # # Learning rate um pouco mais alto permite diminuir o tempo de convergencia.
+    # sgd_manual(lr=1.00 * 10 ** -3)
+    #
+    # # Learning rate alto demais causando a extrapolacao do minimo local e a
+    # # divergencia do gradiente.
+    # sgd_manual(lr=1.62 * 10 ** -3)
 
-    # Learning rate um pouco mais alto permite diminuir o tempo de convergencia.
-    sgd_manual(lr=1.00 * 10 ** -3)
+    sgd_tensorflow(lr=1.00 * 10 ** -4)
 
-    # Learning rate alto demais causando a extrapolacao do minimo local e a
-    # divergencia do gradiente.
-    sgd_manual(lr=1.62 * 10 ** -3)
+    sgd_tensorflow(lr=1.00 * 10 ** -3)
 
 
 if __name__ == "__main__":
